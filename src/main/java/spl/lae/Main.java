740ca846f04ccb321510df03cf9d78e968c164dd
ifatnah <ifatnahmani7@gmail.com>
2025-12-23 16:28:40 +0200

    Mimush_before_test

--- src/main/java/spl/lae/Main.java
+++ src/main/java/spl/lae/Main.java
@@ -1,10 +1,35 @@
 package spl.lae;
 import java.io.IOException;
+import java.text.ParseException;
 
 import parser.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-      // TODO: main
+
+      // Check that args is valid
+      if (args.length != 3 ){
+        System.err.println("The input has to include 3 argumens");
+        return;
+      }
+      // Initialiize parser
+      InputParser parser = new InputParser();
+
+      try{
+        // Use parse function to get root
+        ComputationNode root = parser.parse(args[1]);
+
+        // Get num of threads and change its type to int
+        int treads = Integer.parseInt(args[0]);
+        LinearAlgebraEngine engine = new LinearAlgebraEngine(treads);
+
+        // Initialize result 
+        double[][] result = engine.run(root).getMatrix();
+        OutputWriter.write(result, args[2]);
+      }
+      // Handle errors
+      catch (Exception e){
+        OutputWriter.write(e.getMessage(), args[2]);
+      }
     }
 }
